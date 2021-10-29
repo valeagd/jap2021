78f09d267109db4d1c8477d56b738470a22b5967
valeagd <valentinaaguiarda@gmail.com>
2021-10-29 20:12:51 -0300

    entrega 3 desafiate

--- js/my-profile.js
+++ js/my-profile.js
@@ -47,3 +47,26 @@ function guardarCambios() {
     }
 }
 
+//foto
+function convertir(img) {
+    img.crossOrigin="anonymous";
+    var canvas = document.createElement("canvas");
+    canvas.width = img.width;
+    canvas.height = img.height;
+    var contexto = canvas.getContext("2d");
+    contexto.drawImage(img, 0, 0,img.width, img.height);
+    var dataURL = canvas.toDataURL("image/jpeg");
+    return dataURL;
+  }
+
+  document.addEventListener('DOMContentLoaded',function(){
+  var imgbase64 = convertir(document.getElementById("img"));
+  localStorage.imagen=imgbase64;
+  document.getElementById('imagen2').src=`${localStorage.imagen}`;
+
+  console.log(imgbase64);
+  });
+
+
+  
+  
